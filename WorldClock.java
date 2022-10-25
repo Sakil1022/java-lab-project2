a75a27dae16126daaac5fdd6acbf6e7c70a8f80d
Sakil1022 <116650019+Sakil1022@users.noreply.github.com>
2022-10-25 22:59:50 +0600

    my assignment

---
+++ WorldClock.java
@@ -0,0 +1,14 @@
+public class WorldClock extends Clock {
+    public WorldClock() {
+        super();
+
+
+    }
+
+    @Override
+    public String getHours() {
+        String hours = String.valueOf(Integer.parseInt(super.getHours())+3);
+        return hours;
+    }
+
+}
